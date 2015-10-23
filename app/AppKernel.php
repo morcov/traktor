1c193325161539d0c5bb98a78eea99e5b4685d39
morcov <morcovck@gmail.com>
2015-10-23 10:57:03 +0300

    install doctrine-mongodb

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,8 +14,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
+
             new AppBundle\AppBundle(),
         );
 
