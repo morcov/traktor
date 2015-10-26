807ab50333d9651565a20767c8ad426d80b07103
morcov <morcovck@gmail.com>
2015-10-26 12:24:35 +0200

    detail advert

--- src/AdvertBundle/Controller/DefaultController.php
+++ src/AdvertBundle/Controller/DefaultController.php
@@ -3,10 +3,16 @@ namespace AdvertBundle\Controller;
 
 use AdvertBundle\Form\AdvertType;
 use CommonBundle\Controller\BaseController;
+use Doctrine\ODM\MongoDB\LockException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends BaseController
 {
+    /**
+     * @return Response
+     */
     public function indexAction()
     {
         $adverts = $this->get('advert.advert')->findAll();
@@ -16,6 +22,25 @@ class DefaultController extends BaseController
         ]);
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return Response
+     * @throws LockException
+     */
+    public function detailAction(Request $request, $id)
+    {
+        $advert = $this->get('advert.advert')->find($id);
+
+        return $this->render('@Advert/Default/detail.html.twig', [
+            'advert' => $advert,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
     public function addAction(Request $request)
     {
         $form = $this->getFF()->create(new AdvertType());
@@ -26,7 +51,7 @@ class DefaultController extends BaseController
             $this->getDm()->persist($advert);
             $this->getDm()->flush();
 
-            return $this->redirect($this->get('router')->generate(''));
+            return $this->redirect($this->get('router')->generate('advert_detail', ['id' => $advert->getId()]));
         }
 
         return
